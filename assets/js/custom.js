725472bc0f86314db7d3b2c3a6f2db4da949f968
Nikola Pervic <nikolapervic@me.com>
2018-03-26 07:28:07 +0200

    New arrival, footer and testemonial
    Finished the new arrival section, footer and testemonial. Added some vendor items and animations for the sections.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,89 @@
-console.log(document);
+/**
+ * UI variables
+ */
+var changeTestemonialLeft = document.querySelector('#arrow-left');
+var changeTestemonialRight = document.querySelector('#arrow-right');
+
+changeTestemonialLeft.addEventListener('click', changeLeft);
+changeTestemonialRight.addEventListener('click', changeRight);
+
+function changeLeft(e) {
+    var active = document.querySelector('.show');
+    var passive = document.querySelector('.no-show');
+
+    // Clear the classes
+    active.classList.remove('slideOutRight', 'slideInLeft', 'slideInRight', 'slideOutLeft');
+    passive.classList.remove('slideOutRight', 'slideInLeft', 'slideInRight', 'slideOutLeft');
+
+    // Starts the animation
+    active.classList.add('slideOutLeft');
+    setTimeout(function () {
+
+        // Animates after the initial
+        active.classList.remove('show');
+        active.classList.add('no-show');
+        passive.classList.add('slideInRight');
+        passive.classList.remove('no-show');
+        passive.classList.add('show');
+        
+    }, 500);
+    
+
+}
+
+function changeRight(e) {
+    var active = document.querySelector('.show');
+    var passive = document.querySelector('.no-show');
+
+    // Clear the classes
+    active.classList.remove('slideOutRight', 'slideInLeft', 'slideInRight', 'slideOutLeft');
+    passive.classList.remove('slideOutRight', 'slideInLeft', 'slideInRight', 'slideOutLeft');
+
+    // Starts the animation
+    active.classList.add('slideOutRight');
+    setTimeout(function () {
+
+        // Animates after the initial
+        active.classList.remove('slideOutRight');
+        active.classList.remove('show');
+        active.classList.add('no-show');
+        passive.classList.add('slideInLeft');
+        passive.classList.remove('no-show');
+        passive.classList.add('show');
+
+    }, 500);
+
+
+}
+
+$(function ($) {
+
+    // Call function each time the window is scrolled
+    $(window).scroll(function () {
+
+        // Look for the object
+        $('.products-large-left').each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            // If it is in viewport add the classes
+            if (bottom_of_window > bottom_of_object) {
+                $(this).addClass('animated slideInLeft opacity-full');
+            };
+
+        });
+
+        // Look for the object
+        $('.products-large-right').each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            // If it is in viewport add the classes
+            if (bottom_of_window > bottom_of_object) {
+                $(this).addClass('animated slideInRight opacity-full');
+            };
+
+        });
+
+    });
+});
